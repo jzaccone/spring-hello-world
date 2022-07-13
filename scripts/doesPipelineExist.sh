9d8209787f1ddc1dcdd5b0555c9686b6e7b7abca
John Zaccone <john.zaccone@gmail.com>
2022-07-13 10:57:28 -0400

    adjust script to echo 1 instead of exit 1, since github actions assumes that is a failure

--- scripts/doesPipelineExist.sh
+++ scripts/doesPipelineExist.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 PIPELINE_NAME=spring-build-and-deploy-$1
-echo "Checking if pipeline $PIPELINE_NAME exists"
-PIPELINE_EXISTS=false
+
 for pipeline in `aws codepipeline list-pipelines | yq '.pipelines[].name'`; do
     if [ "$pipeline" = "$PIPELINE_NAME" ]; then
-    PIPELINE_EXISTS=true
-    break
+    echo 1
+    exit
     fi
 done
 
-if [ $PIPELINE_EXISTS ]
-then
-    echo "pipeline exists!"
-    exit 1
-else
-    echo "pipeline does not exist!"
-    exit 0
-fi
+echo 0
